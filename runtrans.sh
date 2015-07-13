7ac3af93b64db951abc9a92a636624bd4991d8ac
jon <jrbelyeu@gmail.com>
2015-07-13 17:34:52 -0600

    Bash script with example inputs to run transpiration.py

---
+++ runtrans.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+python transpiration.py ../site_Files/County_Processed_Sapflux.csv ../site_Files/tradiusMeasurements.csv ../site_Files/tradiusMeasurements.csv ../output/js_output_file_30min.csv ../output/js_output_file_day.csv ../output/et_output_file_30min.csv ../output/et_output_file_day.csv ../output/j0_output_file_day.csv ../output/j0_output_file_day_ave.csv ../output/js_output_file_day_ave.csv ../output/et_output_file_day_ave.csv
